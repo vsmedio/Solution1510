01cd08ed2703e6124131c14bf49d0d14796e5f22
Arvis Abromovics <info@vsmedio.lv>
2018-01-10 23:32:35 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,56 @@
+/* 
+Все лишнее - прочь!
+Убрать в методе main лишние строки, для которых метод add не реализован.
+
+
+Требования:
+1. В классе Solution должно быть реализовано три метода add.
+2. Метод main не должен содержать вызов метода add с параметрами "1" и 2d.
+3. Все методы класса Solution должны быть статическими.
+4. Все методы класса Solution должны быть публичными.
+
+
+public class Solution {
+    public static void main(String[] args) {
+        add((short) 1, 2f);
+        add(1, 2);
+        add(2d, 2);
+        add("1",2d);
+        add((byte) 1, 2d);
+    }
+
+    public static void add(int i, int j) {
+        System.out.println("Integer addition");
+    }
+
+    public static void add(int i, double j) {
+        System.out.println("Integer and double addition");
+    }
+
+    public static void add(double i, double j) {
+        System.out.println("Double addition");
+    }
+}
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        add((short) 1, 2f); //2
+        add(1, 2);       //1
+        add(2d, 2);      //3
+        //add("1",2d);         //error
+        add((byte) 1, 2d);  //2
+    }
+
+    public static void add(int i, int j) {
+        System.out.println("Integer addition");
+    }
+
+    public static void add(int i, double j) {
+        System.out.println("Integer and double addition");
+    }
+
+    public static void add(double i, double j) {
+        System.out.println("Double addition");
+    }
+}
